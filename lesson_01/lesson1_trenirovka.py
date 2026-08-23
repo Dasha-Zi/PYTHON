60c56a7b7f01250f6fe8121c869cbb993d90426f
Dasha-Zi <DashaZi96@gmail.com>
2026-08-23 19:21:21 +0400

    first commit

---
+++ lesson_01/lesson1_trenirovka.py
@@ -0,0 +1,29 @@
+my_heigh =172
+print (my_heigh)
+
+my_name ="Даша"
+my_name ="Даша Зимина"
+print (my_name)
+
+
+pet_name = input ("Как зовут вашего питомца? ")
+print ("Ваш любимчик - "  + pet_name)
+
+
+def print_python():
+    print ("Учу Python!")
+
+print_python ()
+
+
+def print_letter(let):
+    print(let, end='')
+
+
+print_letter('С')
+print_letter('Т')
+print_letter('У')
+print_letter('Д')
+print_letter('Е')
+print_letter('Н')
+print_letter('Т')
