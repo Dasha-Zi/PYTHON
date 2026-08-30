bd3647709edbe9a6ec37a2fb28ced46b4ea93552
Dasha-Zi <DashaZi96@gmail.com>
2026-08-30 12:43:05 +0400

    first commit

---
+++ lesson3/03_lesson/lesson_3_task_1.py
@@ -0,0 +1,7 @@
+from user import User
+
+my_user = User("Dasha", "Zimina")
+
+print(my_user.get_first_name())
+print(my_user.get_last_name())
+print(my_user.get_user_info())
---
+++ lesson3/03_lesson/smartphone.py
@@ -0,0 +1,7 @@
+class Smartphone:
+    def __init__(self, brand, model, nomer):
+                     self.brand = brand
+                     self.model = model
+                     self.nomer = nomer
+
+                     
